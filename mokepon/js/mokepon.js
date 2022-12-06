a0377de1ba8728e6d0bd08ef232c4946e011e24c
Bruno Carrillo <cbruno.carrillo@gmail.com>
2022-12-06 16:56:37 -0600

    Clase número 19 terminada! :D hice unos arreglos al código de la clase

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,23 @@
-function seleccionarMascotaJugador() {
-    alert('Seleccionaste tu mascota');
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota');
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota');
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+function seleccionarMascotaJugador() {
+    let mascotas = document.getElementsByName('mascota');
+    let contador = 0;
+
+    // Así no está en el curso, esto sé me ocurrió a mi XD
+    mascotas.forEach(element => {
+        if(element.checked) {
+            alert('Seleccionaste a ' + element.id);
+        } else {
+            contador++;
+        }
+    });
+    if (contador == 3) {
+        alert('No has seleccionado ningún mokepon');
+    }
+}
+
+window.addEventListener('load', iniciarJuego);
