45a19bf8ce5175ab0a630b9d63571c7ccef5a9a6
Bruno Carrillo <cbruno.carrillo@gmail.com>
2022-12-08 11:25:56 -0600

    VAMOOOOOOOOOOOOOOO, lamentablemente le decimos adiós a un gran profesor de platzi :C pero weno, ahora sí, ya funciona todo genial, ahora vamos a darle diseño B)

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,6 +4,12 @@ let vidasJugador = 3, vidasEnemigo = 3;
 window.addEventListener('load', iniciarJuego);
 
 function iniciarJuego() {
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque');
+    sectionSeleccionarAtaque.style.display = 'none';
+
+    let sectionReiniciar = document.getElementById('reiniciar');
+    sectionReiniciar.style.display = 'none';
+
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 
@@ -19,6 +25,11 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque');
+    sectionSeleccionarAtaque.style.display = 'block';
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota');
+    sectionSeleccionarMascota.style.display = 'none';
+
     let mascotas = document.getElementsByName('mascota');
     let nombreMascotaJugador = document.getElementById('mascota-jugador');
     let contador = 0;
@@ -141,6 +152,9 @@ function crearMensajeFinal(resultadoFinal) {
     botonAgua.disabled = true;
     let botonTierra = document.getElementById('boton-tierra');
     botonTierra.disabled = true;
+
+    let sectionReiniciar = document.getElementById('reiniciar');
+    sectionReiniciar.style.display = 'block';
 }
 
 function reiniciarJuego() {
